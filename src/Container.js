6a6be2545ac904f79ee472d7acda0e5a251f4110
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2017-02-11 20:41:40 +0100

    move Carousel to Home

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router'
-//import './Container.css'
-import Carousel from './Carousel'
 
 export default class extends React.Component {
   render() {
@@ -14,20 +12,7 @@ export default class extends React.Component {
             <li className="color-white"><Link to="/insurance-directory">Insurance Directory</Link></li>
             <li className="color-white"><Link to="/about-us">About Us</Link> </li>
             <li className="color-white"><Link to="/our-offer">Our Offer</Link></li>         
-          </ul>
-          <Link to="/">
-          <img src="images/Logo_White.svg" alt="liimex" width="300px"/>
-          </Link>
-          
-          <Carousel/>
-          <div className="row">
-          <div className="col-md-3">
-          <Link to="/our-offer" className="color-white">learn more</Link>
-          </div>
-          <div className="col-md-3">
-          <Link to="/sign-up" className="color-white">sign up</Link>
-          </div>
-          </div>
+          </ul>          
         </section>
         {this.props.children}
       </div>
