56d92bcc1307dc96b54704883105c48803fec27c
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2017-02-15 03:17:50 +0100

    duplicate routes and components

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import { Router, Route, browserHistory, IndexRedirect, IndexRoute } from 'react-router'
 
-import Container from './Container'
-import App from './App'
-import OurOffer from './OurOffer'
-import AboutUs from './AboutUs'
-import InsuranceDirectory from './InsuranceDirectory'
-import LogIn from './LogIn'
-import SignUp from './SignUp'
+import Container_de from './pages/de/Container'
+import App_de from './pages/de/App'
+import OurOffer_de from './pages/de/OurOffer'
+import AboutUs_de from './pages/de/AboutUs'
+import InsuranceDirectory_de from './pages/de/InsuranceDirectory'
+import LogIn_de from './pages/de/LogIn'
+import SignUp_de from './pages/de/SignUp'
 
+import Container_en from './pages/en/Container'
+import App_en from './pages/en/App'
+import OurOffer_en from './pages/en/OurOffer'
+import AboutUs_en from './pages/en/AboutUs'
+import InsuranceDirectory_en from './pages/en/InsuranceDirectory'
+import LogIn_en from './pages/en/LogIn'
+import SignUp_en from './pages/en/SignUp'
 
 const Routes = props => {
-  const innerRoutes =(
-    <Route path="/" component={Container}>
-        <IndexRoute component={App} />
-        <Route path="/our-offer" component={OurOffer} />
-        <Route path="/about-us" component={AboutUs} />
-        <Route path="/insurance-directory" component={InsuranceDirectory} />
-        <Route path="/log-in" component={LogIn} />
-        <Route path="/sign-up" component={SignUp} />
-    </Route>
-    );
   return (
     <Router history={browserHistory}>
-      {innerRoutes}
+      <Route path="/de" component={Container_de}>
+        <IndexRoute component={App_de} />
+        <Route path="/our-offer" component={OurOffer_de} />
+        <Route path="/about-us" component={AboutUs_de} />
+        <Route path="/insurance-directory" component={InsuranceDirectory_de} />
+        <Route path="/log-in" component={LogIn_de} />
+        <Route path="/sign-up" component={SignUp_de} />
+      </Route>
+      <Route path="/en" component={Container_en}>
+        <IndexRoute component={App_en} />
+        <Route path="/our-offer" component={OurOffer_en} />
+        <Route path="/about-us" component={AboutUs_en} />
+        <Route path="/insurance-directory" component={InsuranceDirectory_en} />
+        <Route path="/log-in" component={LogIn_en} />
+        <Route path="/sign-up" component={SignUp_en} />
+      </Route>
       <Route path="*">
-        <IndexRedirect to="/" />
+        <IndexRedirect to="/de" />
       </Route>
     </Router>
   )
