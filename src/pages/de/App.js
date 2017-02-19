eda679ed14af14104e3b8208ca1e0d8674190876
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2017-02-19 21:18:20 +0100

    rename scss as per the components they are styling

--- src/pages/de/App.js
+++ src/pages/de/App.js
@@ -14,19 +14,21 @@ export default class extends React.Component {
     return (
     	<section className="bg-liimex-blue">
         <Container router={this.props.router} route={this.props.route} setHomeStyle={true}/>
+        <div className="carousel-section">
     	  <Link to="/">
           	<img src="images/Logo_White.svg" alt="liimex" width="300px"/>
-          </Link>
-          <Carousel/>
-          <div className="row">
-	        <div className="col-md-3">
-	        	<Link to="/our-offer" className="color-white">learn more...</Link>
-          	</div>
-          	<div className="col-md-3">
-          		<Link to="/sign-up" className="color-white">sign up</Link>
-          	</div>
-          </div>
-        </section>
+        </Link>
+        <Carousel/>
+        <div className="row">
+          <div className="col-md-3">
+          	<Link to="/our-offer" className="color-white">learn more...</Link>
+        	</div>
+        	<div className="col-md-3">
+        		<Link to="/sign-up" className="color-white">sign up</Link>
+        	</div>
+        </div>
+        </div>
+      </section>
     )
   }
 }
